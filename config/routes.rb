d937b0e9cf2f108186821332ac9562b31e530e33
hoanganhd <doan.hoang.anh@sun-asterisk.com>
2022-06-05 17:16:08 +0700

    Validate phone number, address

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    registrations: 'users/registrations'
   }
 
   get "/home", to: "home#index"
