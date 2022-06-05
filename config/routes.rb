5fb9748f3c28622180ae90f0b40a1042901711e4
hoanganhd <doan.hoang.anh@sun-asterisk.com>
2022-06-05 16:24:46 +0700

    Log out feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   get "/home", to: "home#index"
 end
